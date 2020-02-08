123f25dbc314bb0b06448d4313511cf2fbdec9b6
Lian Yelich <lianyelich14@gmail.com>
2020-02-08 22:32:06 +0000

    Login/logout functionality

--- public/scripts/client_add_item.js
+++ public/scripts/client_add_item.js
@@ -1,9 +1,5 @@
 $(document).ready(()=> {
 
-   $('header').on('click','.prevention',function(event) {
-     event.preventDefault();
-   });
-
   $('#menu-container').on('click', '.prevention',function(event) {
     event.preventDefault();
     console.log($(this).siblings()[0]);
