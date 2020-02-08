6f9c7be3dec919f08c406119dfe9a179f16290d8
Lian Yelich <lianyelich14@gmail.com>
2020-02-08 21:57:33 +0000

    Add item to card logic

--- public/scripts/client_add_item.js
+++ public/scripts/client_add_item.js
@@ -6,7 +6,19 @@ $(document).ready(()=> {
 
   $('#menu-container').on('click', '.prevention',function(event) {
     event.preventDefault();
-    console.log($(this).siblings()[0]);
+    // console.log($(this).siblings()[0]);
+    const $cartName = $('<td>')
+    .text($(this).siblings()[0]);
+    const $cartPrice = $('<td>')
+    .text($(this).siblings()[1]);
+    const $cartCalories = $('<td>')
+    .text($(this).siblings()[2]);
+
+    const $cartItem = $('<tr>')
+    .append($cartName,$cartPrice,$cartCalories);
+
+    const $shoppingCart = $('#shopping-cart')
+    .append($cartItem);
   });
 
 });
