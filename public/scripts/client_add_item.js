b3b70d57b2cc737f848e026408aead4fc8b4042f
Lian Yelich <lianyelich14@gmail.com>
2020-02-10 00:27:39 +0000

    Create genereateShoppingCartTable function

--- public/scripts/client_add_item.js
+++ public/scripts/client_add_item.js
@@ -1,10 +1,11 @@
+let shoppingCartObject = [];
 $(document).ready(()=> {
 
   $('#menu-container').on('click', '.prevention',function(event) {
     event.preventDefault();
     for (let i = 0; i < $('#shopping-cart').length; i++) {
       console.log($('#shopping-cart tr').length - 2)
-      console.log($('.item-name').find(`${$(this).siblings()[0].innerHTML}`).innerHTML);
+      console.log($('.item-name').find(`${$(this).siblings()[0].innerHTML}`));
       if (($(this).siblings()[0].innerHTML) === $('.item-name').text()) {
         console.log($('.item-name')[i].innerHTML);
       }
@@ -31,11 +32,38 @@ $(document).ready(()=> {
   $('.hide-cart').on('click', ()=>{
     event.preventDefault();
      $('#shopping-div').css('visibility', 'hidden')
-     
+
   })
-  
+
 //  $("#checkout-form").on('submit', () => {
 
 //  })
-
+  const generateShoppingCartTable = function(array) {
+    const totalPrice = 0;
+    const $tbody = $('#item-body');
+    $tbody.empty();
+    for (const item in array) {
+      totalPrice += item.price;
+      $('#shopping-div').css("visibility","visible");
+      const $itemName = $('<td>')
+      .addClass('item-name')
+      .text(item.name);
+      const $itemPrice = $('<td>')
+      .text(item.price);
+      const $itemCalories = $('<td>')
+      .text(item.calories);
+      const $itemQuantity = $('<td>')
+      .text(1);
+      const $rowItem = $('<tr>')
+      .append($itemName,$itemPrice,$itemCalories,$itemQuantity);
+      $tbody.prepend($rowItem);
+    }
+    const $totalData = $('<td>')
+    .text('Total')
+    const $totalPrice = $('<td>')
+    .text(totalPrice)
+    const $totalRow = $('<tr>')
+    .append($totalData,$totalPrice)
+    $tbody.append($totalRow);
+  };
 });
