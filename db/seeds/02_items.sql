f22ad51de83d053571a802f8f7d881c98c9fbdda
Lian Yelich <lianyelich14@gmail.com>
2020-02-11 17:44:05 +0000

    add more items/unique images

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -1,4 +1,9 @@
 INSERT INTO items (price, calories, name, picture)VALUES
-(9.99, 1999, 'Big Dak', 'https://image.shutterstock.com/image-photo/small-juicy-hamburger-canapes-on-260nw-570368920.jpg'),
-(19.99, 1888, 'mcTriple', 'https://image.shutterstock.com/image-photo/small-juicy-hamburger-canapes-on-260nw-570368920.jpg'),
-(29.99, 1777, 'DakFries', 'https://image.shutterstock.com/image-photo/small-juicy-hamburger-canapes-on-260nw-570368920.jpg');
+(9.99, 1999, 'Big Dac', 'https://image.shutterstock.com/image-photo/small-juicy-hamburger-canapes-on-260nw-570368920.jpg'),
+(19.99, 1888, 'DacTriple', '/images/dacMonaldsTriple.png'),
+(29.99, 1777, 'DacFries', '/images/dacMonaldsFries.png'),
+(5.99, 698, 'DacShake', '/images/dacMonaldsMilkshake.png');
+-- (),
+-- (),
+-- (),
+-- ();
