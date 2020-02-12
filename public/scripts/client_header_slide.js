272e8e8a833ccf334c3859eb939b53904494abe7
Nick <ndanvers222@gmail.com>
2020-02-12 05:39:51 +0000

    footer bar, button w/ jquery slidetoggle for shopping cart

--- public/scripts/client_header_slide.js
+++ public/scripts/client_header_slide.js
@@ -3,12 +3,17 @@ $(()=> {
    event.preventDefault();
    $("#under-nav-div").slideToggle();
   })
+
+
   $('#header-track-button').click(function(event) {
     event.preventDefault();
     $("#under-nav-div").slideToggle();
    })
 
-
+  $('#footer-button').click(function(event) {
+    event.preventDefault();
+    $("#shopping-div").slideToggle();
+   })
 }) 
 
 
