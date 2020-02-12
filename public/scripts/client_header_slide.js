57e6faceb43e239fd2757fe1a402a35bdd56b398
Nick <ndanvers222@gmail.com>
2020-02-12 02:40:51 +0000

    attempted displaynone-> conflicted with the flex

--- public/scripts/client_header_slide.js
+++ public/scripts/client_header_slide.js
@@ -3,6 +3,10 @@ $(()=> {
    event.preventDefault();
    $("#under-nav-div").slideToggle();
   })
+  $('#header-track-button').click(function(event) {
+    event.preventDefault();
+    $("#under-nav-div").slideToggle();
+   })
 
 
 }) 
